5ec243f5ef5985ce816f67c683cbc1f7f408a445
olesherka <smoke.paladin@gmail.com>
2024-06-23 22:41:06 +0600

    last commit

--- src/main/java/com/example/bunker/repository/UserRepository.java
+++ src/main/java/com/example/bunker/repository/UserRepository.java
@@ -6,10 +6,22 @@ import com.example.bunker.models.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     @Query(value = "SELECT u FROM User u WHERE u.id = :id")
     User findByUsername(@Param("username") String username);
+
+    @Transactional
+    void deleteById(Integer id);
+
+    @Query(value = "SELECT * FROM User u", nativeQuery = true)
+    List<User> getAllUsers();
+
+    @Query(value = "SELECT u FROM User u WHERE u.id = :id")
+    User  findById(@Param("user_id") int id);
 }
 
